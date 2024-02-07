2dbbce50ee72c793fbd9a5d9a0d303307a5bc770
zennon-sml <zennonsampaioml@gmail.com>
2024-02-07 10:55:22 -0300

    modal and modal style

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,34 @@
-export function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
+
+const note = {
+  date: new Date(),
+  content: "asdf"
 }
 
+export function App() {
+  return (
+    <div className="mx-auto max-w-6xl ny-12 space-y-6">
+      <img src={logo} alt="NLW" />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas"
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+
+          <NewNoteCard />
+
+          <NoteCard note={note}/>
+        
+      </div>
+    </div>
+  );
+}
