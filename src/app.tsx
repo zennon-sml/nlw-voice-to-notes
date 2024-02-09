20a3012bc2774648a5a7e404a6e29883268065a1
zennon-sml <zennonsampaioml@gmail.com>
2024-02-09 16:48:46 -0300

    delete note

--- src/app.tsx
+++ src/app.tsx
@@ -9,45 +9,55 @@ interface Note {
   content: string;
 }
 
-
 export function App() {
   const [search, setSearch] = useState("");
   const [notes, setNotes] = useState<Note[]>(() => {
     const notesOnStorage = localStorage.getItem("notes");
-    
+
     if (notesOnStorage) {
       return JSON.parse(notesOnStorage);
     }
-    
+
     return [];
   });
-  
+
   function onNoteCreated(content: string) {
     const newNote = {
       id: crypto.randomUUID(),
       date: new Date(),
       content,
     };
-    
+
     const notesArray = [newNote, ...notes];
-    
+
     setNotes(notesArray);
-    
+
     localStorage.setItem("notes", JSON.stringify(notesArray));
   }
-  
+
+  function onNoteDelete(id: string) {
+    const deletedNotesArray = notes.filter((note) => {
+      return note.id !== id;
+    });
+
+    setNotes(deletedNotesArray);
+
+    localStorage.setItem("notes", JSON.stringify(deletedNotesArray));
+  }
+
   function handleSearch(event: ChangeEvent<HTMLInputElement>) {
     const query = event.target.value;
-    
+
     setSearch(query);
   }
-  const filteredNotes = search !== ""
-  ? notes.filter(note => note.content.includes(search))
-  : notes
-  
-  
+
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) => note.content.includes(search))
+      : notes;
+
   return (
-    <div className="mx-auto max-w-6xl ny-12 space-y-6">
+    <div className="mx-auto max-w-6xl ny-12 space-y-6 px-5">
       <img src={logo} alt="NLW" />
 
       <form className="w-full">
@@ -61,10 +71,13 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
+
         {filteredNotes.map((note) => {
-          return <NoteCard key={note.id} note={note} />;
+          return (
+            <NoteCard key={note.id} note={note} onNoteDelete={onNoteDelete} />
+          );
         })}
       </div>
     </div>
